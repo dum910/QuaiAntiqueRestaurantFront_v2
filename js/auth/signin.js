8c72fddb1f9922bb881941a95cbb45d924f311f3
Cédric DUMEAUX <c_dumeaux@outlook.fr>
2024-09-23 12:13:10 +0200

    Liaison de la connexion entre la page de connection et l'API

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,22 +1,44 @@
 const mailInput = document.getElementById('EmailInput');
 const passwordInput = document.getElementById('PasswordInput');
 const btnSignin = document.getElementById('BtnSignin');
+const signinForm = document.getElementById('signinForm');
 
 btnSignin.addEventListener('click', checkCredentials);
 
 function checkCredentials() {
-    //Ici, il faudra appelé l'API pour vérifier les identifiants en BDD
+    let dataform = new FormData(signinForm);
 
-    if (mailInput.value === "test@mail.com" && passwordInput.value === "123") {
-        //alert("Vous êtes connecté");
-        //il faudra récupérer le vrai token de l'API
-        const token = "fjnqevilrenbvlewhrbclwbqlerkvbnwlvisnd"
-        setToken(token);
-        //Placer ce token en cookie
-        setCookie(roleCookieName, "client", 7)
-        window.location.replace("/");
-    } else {
-        mailInput.classList.add("is-invalid");
-        passwordInput.classList.add("is-invalid");
-    }
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        "username": dataform.get("email"),
+        "password": dataform.get("mdp")
+    });
+
+    let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch(apiUrl + "login", requestOptions)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                mailInput.classList.add("is-invalid");
+                passwordInput.classList.add("is-invalid");
+            }
+        })
+        .then(result => {
+            const token = result.apiToken;
+            setToken(token);
+            //Placer ce token en cookie
+            setCookie(roleCookieName, result.roles[0], 7)
+            window.location.replace("/");
+        })
+        .catch(error => console.log('error', error));
+
 }
